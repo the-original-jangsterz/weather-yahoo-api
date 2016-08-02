20408d57acc3f1ecdef65d140200be92bd0714b5
Peter Jang <peter.x.jang@gmail.com>
2016-08-01 20:58:43 -0500

    Add temperature and 5 day forecast

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,6 +1,9 @@
 class ForecastsController < ApplicationController
   def index
     @data = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
+    @temperature = @data["query"]["results"]["channel"]["item"]["condition"]["temp"]
+    @description = @data["query"]["results"]["channel"]["item"]["description"]
+    @forecast_days = @data["query"]["results"]["channel"]["item"]["forecast"]
     render 'index.html.erb'
   end
 end
